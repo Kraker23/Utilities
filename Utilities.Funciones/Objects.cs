0040c9caf8d4a2f2f05bea4f37a1c4419b585ff5
Kraker23 <41199710+Kraker23@users.noreply.github.com>
2022-04-21 09:42:20 +0200

    Comprobacion de Si existen nulos

---
+++ Utilities.Funciones/Objects.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Utilities.Funciones
+{
+    /// <summary> Clase para agrupar funciones matemáticas </summary>
+    public class Objects
+    {
+        ///Comprobacion de Si existen nulos
+        public static bool TieneNulos(params object[] args)
+        {
+            foreach (var item in args)
+            {
+                if (item == null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        ///Comprobacion de Si existen nulos
+        public static bool  TieneNulos(string nombres, out string mensaje, params object[] args)//
+        {
+            string[] nombresSplit=null;
+            bool hayNombres = false; 
+            if (!string.IsNullOrEmpty(nombres))
+            {
+                hayNombres = true;
+                nombresSplit = nombres.Split(',');
+            }
+
+            string nombresNulos = string.Empty;
+            bool existeNulos = false;
+            mensaje = null; ;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == null)
+                {
+                    existeNulos = true;
+                    if (hayNombres)
+                    {
+                        if (nombresSplit.Length > i)
+                        {
+                            nombresNulos = Concatenar(nombresNulos, nombresSplit[i]);
+                        }
+                    }
+                }
+            }
+
+            if (existeNulos)
+            {
+                mensaje = "Existen Variables con Nulos";
+                if (!string.IsNullOrEmpty(nombresNulos))
+                {
+                    mensaje = Concatenar(mensaje, nombresNulos, " : ");
+                }
+                return true;
+            }
+            return false;
+        }
+       
+    }
+}
