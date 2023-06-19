f1676e1095a28c13f3f97b9d556c75846bc275e8
Kraker23 <41199710+Kraker23@users.noreply.github.com>
2023-06-19 08:42:49 +0200

    GetDescription Enum

--- Utilities.Extensions/EnumExtensions.cs
+++ Utilities.Extensions/EnumExtensions.cs
@@ -59,5 +59,13 @@ namespace Utilities.Extensions
                 return "Excepción de enumeración";
             }
         }
+        
+         public static string GetDescription(this Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+            if (fieldInfo == null) return null;
+            var attribute = (DescriptionAttribute)fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute));
+            return attribute.Description;
+        }
     }
 }
